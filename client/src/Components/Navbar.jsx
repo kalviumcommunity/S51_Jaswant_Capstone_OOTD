1e8bd7bafe43b2271fe33b1e02f6792f9216b83b
Jaswa07 <jaswaramesh2005@gmail.com>
2024-03-14 14:02:21 +0530

    added navbar

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className="nav-link">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/features" className="nav-link">Features</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/Login" className="nav-link">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
