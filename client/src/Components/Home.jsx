91898c8f180c38f943e932075df14a352a0bd7ae
Jaswa07 <jaswaramesh2005@gmail.com>
2024-03-19 14:32:36 +0530

    homepage done

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./Home.css"
 
 const Home = () => {
     return (
         <div>
-            <h2>Home Page</h2>
-            <p>Welcome to the home page.</p>
+            <h1>Welcome to Outfit Of The Day</h1>
+            <p>"What you wear is how you present yourself to the world, especially today,<br></br> when human contacts are so quick. Fashion is instant language."</p>
         </div>
     );
 };
