d6385f049c9aec234c6cf7ab0ec225a505b42458
Jaswa07 <jaswaramesh2005@gmail.com>
2024-03-19 13:56:37 +0530

    ok

--- Server.js
+++ Server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require ("mongoose")
+const {Connect, isConnected} = require("./db")
+require('dotenv').config()
 
 const port = 3000
 
-app.get("/", (res, req) => {
-  req.send({ message: "Hello World!" });
+Connect()
+
+app.get("/", (req, res) => {
+  const htmlResponse = `<h1><i>OOTD</i></h1><p>Database Connection Status: ${isConnected ? 'Connected' : 'Disconnected'}</p>`;
+  res.send(htmlResponse);
 });
 
 app.listen(port, async () => {
   console.log(`🚀 server running on PORT: ${port}`);
+});
+
+mongoose.connection.once('open', () => {
+  console.log("Connected to mongoDB");
 });
