4c5eb8a53e5b06f2980067cf263f3ecae2a8a43a
Jaswa07 <jaswaramesh2005@gmail.com>
2024-04-04 14:58:39 +0530

    -added logo

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import "./Footer.css"
+import twitterLogo from  "../assets/twitter.png"   // import Twitter logo image
+import facebookLogo from '../assets/facebook.png'; // import Facebook logo image
+import instagramLogo from '../assets/insta.png'; // import Instagram logo image
 
 const Footer = () => {
     return (
         <footer className="home-footer">
             <p>&copy; {new Date().getFullYear()} Outfit Of The Day. All rights reserved.</p>
-            <p>Follow us on:
-                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"> Twitter</a>,
-                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"> Facebook</a>,
-                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"> Instagram</a>
-            </p>
+           
+            <div className='sociallogo'>
+            <p>Follow us on:</p>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                    <img height="80px" src={twitterLogo} alt="Twitter"  />
+                </a>
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <img height="80px"  src={facebookLogo} alt="Facebook" />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <img height="80px" src={instagramLogo} alt="Instagram" />
+                </a>
+                </div>
         </footer>
     );
 }
