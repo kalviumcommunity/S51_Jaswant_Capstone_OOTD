7954bb441c0e45ca45271bad405239d08d0ec9b5
Jaswa07 <jaswaramesh2005@gmail.com>
2024-04-08 14:28:59 +0530

    pasted the logo

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; 
+import logo from "../assets/logo.png"; 
+
 const Navbar = ({isLoggedIn, setIsLoggedIn}) => {
     const handleLogout = () => {
         setIsLoggedIn(false);
-
     };
+
     return (
         <nav className="navbar">
+             <li className="nav-item">
+                    <img src={logo} alt="Logo" className="nav-logo" />
+                </li>
             <ul className="navbar-nav">
                 <li className="nav-item">
                     <Link to="/" className="nav-link">Home</Link>
