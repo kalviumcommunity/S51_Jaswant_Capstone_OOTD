889728675d8fb14e2bba17439e748c6c03030706
Jaswa07 <jaswaramesh2005@gmail.com>
2024-04-12 15:25:30 +0530

    changed footer ui

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -7,18 +7,18 @@ import instagramLogo from '../assets/insta.png'; // import Instagram logo image
 const Footer = () => {
     return (
         <footer className="home-footer">
-            <p>&copy; {new Date().getFullYear()} Outfit Of The Day. All rights reserved. </p>
+            <p className='foot'>&copy; {new Date().getFullYear()} Outfit Of The Day. All rights reserved. </p>
            
             <div className='sociallogo'>
-            <p>Follow us on:</p>
+            <p className='foot'>Follow us on:</p>
             <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                    <img height="80px" src={twitterLogo} alt="Twitter"  />
+                    <img height="60px" src={twitterLogo} alt="Twitter"  />
                 </a>
                 <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                    <img height="80px"  src={facebookLogo} alt="Facebook" />
+                    <img height="60px"  src={facebookLogo} alt="Facebook" />
                 </a>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                    <img height="80px" src={instagramLogo} alt="Instagram" />
+                    <img height="60px" src={instagramLogo} alt="Instagram" />
                 </a>
                 </div>
         </footer>
