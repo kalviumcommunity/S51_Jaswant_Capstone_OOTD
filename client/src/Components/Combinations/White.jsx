ebad33bf38c0521fbe969b7a5d47c6f1e166f6df
Jaswa07 <jaswaramesh2005@gmail.com>
2024-04-18 15:34:33 +0530

    added images

--- client/src/Components/Combinations/White.jsx
+++ client/src/Components/Combinations/White.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './White.css'
+import image1 from '../../assets/whiteimage1.jpeg';
+import image2 from '../../assets/whiteimage2.jpeg';
+import image3 from '../../assets/whiteimage3.jpeg';
+import image4 from '../../assets/whiteimage4.jpeg';
+import image5 from '../../assets/whiteimage5.jpeg';
 
 export const White = () => {
   return (
-    <div>White</div>
-  )
-}
+    <div className="white-container"> 
+      <img src={image1} alt="Image 1" className="white-image" /> 
+      <img src={image2} alt="Image 2" className="white-image" />
+      <img src={image3} alt="Image 3" className="white-image" />
+      <img src={image4} alt="Image 4" className="white-image" />
+      <img src={image5} alt="Image 5" className="white-image" />
+    </div>
+  );
+};
