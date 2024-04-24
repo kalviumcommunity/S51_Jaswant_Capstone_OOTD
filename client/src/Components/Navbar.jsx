93d7150daa6b410e0be1484ab9805adc8e864d80
Jaswa07 <jaswaramesh2005@gmail.com>
2024-04-24 17:05:45 +0530

    changed login button

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; 
-import logo from "../assets/logo.png"; 
+import './Navbar.css';
+import logo from "../assets/logo.png";
 
-const Navbar = ({isLoggedIn, setIsLoggedIn}) => {
+const Navbar = ({ isLoggedIn, setIsLoggedIn }) => {
     const handleLogout = () => {
         setIsLoggedIn(false);
     };
 
     return (
         <nav className="navbar">
-             <li className="nav-item">
-                    <img src={logo} alt="Logo" className="nav-logo" />
-                </li>
-                <div className="search-bar">
+            <li className="nav-item">
+                <img src={logo} alt="Logo" className="nav-logo" />
+            </li>
+            <div className="search-bar">
                 <input type="text" placeholder="Search..." className="search-input" />
                 <button className="search-button">Search</button>
             </div>
@@ -28,10 +28,27 @@ const Navbar = ({isLoggedIn, setIsLoggedIn}) => {
                     <Link to="/features" className="nav-link">Combinations</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/Login" className="nav-link">Login</Link>
+
+
+
+                    <div class="dropdown">
+                        <div class="dropdown-toggle">
+                            <div class="line"></div>
+                            <div class="line"></div>
+                            <div class="line"></div>
+                        </div>
+                        <div class="dropdown-menu">
+                            <Link to="/Login" className="nav-link">Login</Link>
+                            <br />
+                            <hr />
+                            <Link className="nav-link">Save</Link>
+                        </div>
+                    </div>
+
+
                 </li>
             </ul>
-           
+
         </nav>
     );
 };
