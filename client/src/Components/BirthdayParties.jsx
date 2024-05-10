d756dd0add9638bee4609a35191c0490a3a8505b
Jaswaa <141898693+Jaswaa@users.noreply.github.com>
2024-05-10 14:54:36 +0530

    added images

--- client/src/Components/BirthdayParties.jsx
+++ client/src/Components/BirthdayParties.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Bdayparty.css";
+import "./Occasion.css";
 import image1 from '../assets/Bdayimg1.jpeg'
 import image2 from '../assets/Bdayimg2.jpeg'
 import image3 from '../assets/Bdayimg3.jpeg'
@@ -9,11 +9,11 @@ export const BirthdayParties = () => {
   return (
     <>
     <h1>Birthday Party Outfits</h1>
-    <div className='Bday-container'>
-      <img src={image1} alt="Image1" className="Bdayimg" />
-      <img src={image2} alt="Image1" className="Bdayimg" />
-      <img src={image3} alt="Image1" className="Bdayimg" />
-      <img src={image4} alt="Image1" className="Bdayimg" />
+    <div className='occasion-container'>
+      <img src={image1} alt="Image1" className="occasionimg" />
+      <img src={image2} alt="Image1" className="occasionimg" />
+      <img src={image3} alt="Image1" className="occasionimg" />
+      <img src={image4} alt="Image1" className="occasionimg" />
     </div>
     </>
   )
--- client/src/Components/Office.jsx
+++ client/src/Components/Office.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './Black.css';
+import './Combinations/Combination.css';
 import image1 from '../assets/Officeimg1.jpg';
 import image2 from '../assets/Officeimg2.jpg';
 import image3 from '../assets/Officeimg3.jpg';
@@ -12,16 +12,16 @@ import image9 from '../assets/Officeimg9.jpg';
 
 export const Office = () => {
   return (
-    <div className="Black-container"> 
-      <img src={image1} alt="Image 1" className="Black-image" /> 
-      <img src={image2} alt="Image 2" className="Black-image" />
-      <img src={image3} alt="Image 3" className="Black-image" />
-      <img src={image4} alt="Image 4" className="Black-image" />
-      <img src={image5} alt="Image 5" className="Black-image" />
-      <img src={image6} alt="Image 6" className="Black-image" />
-      <img src={image7} alt="Image 7" className="Black-image" />
-      <img src={image8} alt="Image 8" className="Black-image" />
-      <img src={image9} alt="Image 9" className="Black-image" />
+    <div className="combination-container"> 
+      <img src={image1} alt="Image 1" className="combination-image" /> 
+      <img src={image2} alt="Image 2" className="combination-image" />
+      <img src={image3} alt="Image 3" className="combination-image" />
+      <img src={image4} alt="Image 4" className="combination-image" />
+      <img src={image5} alt="Image 5" className="combination-image" />
+      <img src={image6} alt="Image 6" className="combination-image" />
+      <img src={image7} alt="Image 7" className="combination-image" />
+      <img src={image8} alt="Image 8" className="combination-image" />
+      <img src={image9} alt="Image 9" className="combination-image" />
     </div>
   );
 };
