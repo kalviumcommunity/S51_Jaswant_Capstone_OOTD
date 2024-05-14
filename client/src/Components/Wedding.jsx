65a4bd76031e1782cdfb5375b6bdddad3c3def23
Jaswaa <141898693+Jaswaa@users.noreply.github.com>
2024-05-14 14:12:01 +0530

    added images

--- client/src/Components/Wedding.jsx
+++ client/src/Components/Wedding.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import "./Occasion.css";
+import image1 from '../assets/Marriageimg1.jpeg'
+import image2 from '../assets/Marriageimg2.jpeg'
+import image3 from '../assets/Traditionalimg2.jpg'
+import image4 from '../assets/Marriageimg3.jpeg'
+import image5 from '../assets/Marriageimg4.jpeg'
+import image6 from '../assets/Marriageimg5.jpeg'
+import image7 from '../assets/Marriageimg6.jpeg'
+import image8 from '../assets/Marriageimg7.jpeg'
+import image9 from '../assets/Marriageimg8.jpeg'
+import image10 from '../assets/Marriageimg9.jpeg'
+import image11 from '../assets/Marriageimg10.jpeg'
+import image12 from '../assets/Marriageimg11.jpeg'
+import image13 from '../assets/Marriageimg12.jpeg'
+import image14 from '../assets/Marriageimg13.jpeg'
+import image15 from '../assets/Marriageimg14.jpeg'
+import image16 from '../assets/Marriageimg15.jpeg'
+import image17 from '../assets/Marriageimg16.jpeg'
+import image18 from '../assets/Marriageimg17.jpeg'
 
 export const Wedding = () => {
   return (
-    <div>Wedding
+    <>
+    <h1>Wedding Outfits</h1>
+    <h2>Marriage Outfits</h2>
+    <div className='occasion-container'>
+      <img src={image1} alt="Image1" className="occasionimg" />
+      <img src={image2} alt="Image1" className="occasionimg" />
+      <img src={image3} alt="Image1" className="occasionimg" />
+      <img src={image4} alt="Image1" className="occasionimg" />
+      <img src={image5} alt="Image1" className="occasionimg" />
+      <img src={image6} alt="Image1" className="occasionimg" />
+      <img src={image7} alt="Image1" className="occasionimg" />
     </div>
-    
+    <hr className="thick-dark-line" />
+    <h2>Wedding Reception Outfits</h2>
+    <img src={image8} alt="Image1" className="occasionimg" />
+    <img src={image9} alt="Image1" className="occasionimg" />
+    <img src={image10} alt="Image1" className="occasionimg" />
+    <img src={image11} alt="Image1" className="occasionimg" />
+    <img src={image12} alt="Image1" className="occasionimg" />
+    <img src={image13} alt="Image1" className="occasionimg" />
+    <img src={image14} alt="Image1" className="occasionimg" />
+    <img src={image15} alt="Image1" className="occasionimg" />
+    <img src={image16} alt="Image1" className="occasionimg" />
+    <img src={image17} alt="Image1" className="occasionimg" />
+    <img src={image18} alt="Image1" className="occasionimg" />
+    </>
   )
 }
